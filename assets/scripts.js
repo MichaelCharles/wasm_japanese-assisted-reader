4870d599d9b2c602fe4d7fadcad23bfa16e7d89b
Michael Charles Aubrey <m-aubrey@maki.ne.jp>
2023-02-06 21:45:54 +0900

    use jquery directly

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,56 +1,38 @@
-const sampleTitle = $("#x-title").text().trim();
-const sampleBody = $("#x-body").text().trim();
-
 $(async () => {
-  const $loadText = $("#load-text");
-  const $parse = $("#parse");
-  const $sample = $("#sample");
-  const $clear = $("#clear");
-  const $titleInput = $("#title-input");
-  const $bodyInput = $("#body-input");
-  const $reader = $("#reader");
-  const $input = $("#input");
-  const $title = $("#title");
-  const $body = $("#body");
-  const $spinner = $("#spinner-container");
-  const $main = $("main");
-
-  $loadText.hide();
-  $reader.hide();
+  $("#load-text").hide();
+  $("#reader").hide();
 
   const worker = await wasmWorker("lib.wasm", ["read"]);
-  $spinner.fadeOut(() => {
-    $main.fadeIn();
+  $("#spinner-container").fadeOut(() => {
+    $("main").fadeIn();
   });
 
-  $parse.on("click", () => {
-    $input.fadeOut(async () => {
-      const titleResult = await worker.read($titleInput.val());
-      const bodyResult = await worker.read($bodyInput.val());
-      $title.html(titleResult);
-      $body.html(bodyResult);
-      $reader.fadeIn(() => {
-        $loadText.fadeIn();
+  $("#parse").on("click", () => {
+    $("#input").fadeOut(async () => {
+      $("#title").html(await worker.read($("#title-input").val()));
+      $("#body").html(await worker.read($("#body-input").val()));
+      $("#reader").fadeIn(() => {
+        $("#load-text").fadeIn();
       });
     });
   });
 
-  $sample.on("click", () => {
-    $titleInput.val(sampleTitle);
-    $bodyInput.val(sampleBody);
+  $("#sample").on("click", () => {
+    $("#title-input").val($("#x-title").text().trim());
+    $("#body-input").val($("#x-body").text().trim());
   });
 
-  $clear.on("click", () => {
-    $titleInput.val("");
-    $bodyInput.val("");
+  $("#clear").on("click", () => {
+    $("#title-input").val("");
+    $("#body-input").val("");
   });
 
-  $loadText.on("click", () => {
-    $loadText.fadeOut();
-    $reader.fadeOut(() => {
-      $title.html("");
-      $body.html("");
-      $input.fadeIn();
+  $("#load-text").on("click", () => {
+    $("#load-text").fadeOut();
+    $("#reader").fadeOut(() => {
+      $("#title").html("");
+      $("#body").html("");
+      $("#input").fadeIn();
     });
   });
 });
